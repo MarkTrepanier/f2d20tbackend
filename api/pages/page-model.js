84c8c848802d87cacbaa02d3e8c0447e399fe6d7
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-11-22 19:16:38 -0500

    page return shape modified

---
+++ api/pages/page-model.js
@@ -0,0 +1,45 @@
+const db = require("../data/db-config");
+
+
+
+async function insertPage(page) {
+  const [newPageObject] = await db("pages")
+    .insert(page, ["page_id", "text"])
+    .select("page_id", "pagename");
+  return newPageObject;
+}
+
+async function findById(page_id) {
+  const page = await db("pages as p")
+  .leftJoin("options as o","p.page_id","o.page_id")
+  .select( "p.text", "o.text as option", "o.link_id as link","p.page_id")
+  .where( "o.page_id",page_id )
+
+  const result = {
+    options: [],
+  };
+  for (let option of page) {
+    if (!result.page_id) {
+      result.page_id = option.page_id;
+      result.text = option.text;
+    }
+    if (option.option) {
+      result.options.push({
+        option: option.option,
+        link: option.link,
+      });
+    }
+  }
+
+  return result;
+}
+
+function findBy(filter) {
+  return db("pages").where(filter);
+}
+
+module.exports = {
+  insertPage,
+  findBy,
+  findById,
+};
--- api/users/userRouter.js
+++ api/users/userRouter.js
@@ -1,11 +1,21 @@
 const router = require("express").Router();
 const User = require("./userModel");
+const Page = require("../pages/page-model");
 
 router.get("/", async (req, res) => {
   res.json(await User.getAllUsers());
 });
 
 //router.get("/:user_id", {});
+//get page
+router.get("/:user_id/pages/:page_id", async (req,res,next)=>{
+  try{
+    const page = await Page.findById(req.params.page_id)
+    res.status(200).json(page)
+  }catch(err){
+    next(err)
+  }
+})
 
 //router.get("/:user_id/invites", {});
 
